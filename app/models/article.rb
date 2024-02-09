f40a0e20c20378a0d80b3b7232974707950904c8
Louise Dungca <dungca.louisechristine@gmail.com>
2024-02-09 20:54:33 +0800

    feat: add edit view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,12 +2,11 @@ class Article < ApplicationRecord
   validates :title, presence: { message: "Field is required. Enter a title no longer than 50 characters." },
                     length: { maximum: 50 }
   validates :body, presence: { message: "Field is required." }
-  validates :author_name, presence: { message: "Field is required." }
-  # validate :default_author_name
+  validate :default_author_name
 
-  # private
+  private
 
-  # def default_author_name
-  #   self.author_name = "Anonymous" if author_name.blank?
-  # end
+  def default_author_name
+    self.author_name = "Anonymous" if author_name.blank?
+  end
 end
